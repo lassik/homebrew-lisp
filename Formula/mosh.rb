2b3cb9fafadec27db2b913650a30a7545ccd815b
mytoh <mytoh@gmail.com>
2012-11-07 19:16:21 +0900

    first commit

---
+++ Formula/mosh.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+class Mosh < Formula
+  homepage 'http://mosh.monaos.org'
+  url 'http://mosh-scheme.googlecode.com/files/mosh-0.2.7.tar.gz'
+  sha1 '866c08ac12e14733ce27756001a27257624d01ad'
+  head 'https://github.com/okuoku/mosh.git'
+
+  depends_on 'gmp'
+  depends_on 'oniguruma'
+
+  fails_with :llvm do
+    cause "Inline asm errors"
+  end
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+end
