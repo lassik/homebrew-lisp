c44903f9eeb9e51fd223959c670db7ac10c6e59e
Alex Arslan <ararslan@comcast.net>
2017-05-30 11:00:15 -0700

    Update Owl

--- Formula/owl-lisp.rb
+++ Formula/owl-lisp.rb
@@ -1,9 +1,9 @@
 class OwlLisp < Formula
   desc "A functional Lisp"
   homepage "https://github.com/aoh/owl-lisp"
-  url "https://github.com/aoh/owl-lisp/archive/v0.1.13.tar.gz"
-  version "0.1.13"
-  sha256 "090ef90af5f11e7bf7751b67ca96d5cad3b405391966a96a888db8adb9b69978"
+  url "https://github.com/aoh/owl-lisp/archive/v0.1.14.tar.gz"
+  version "0.1.14"
+  sha256 "4d9982da3582456d1e769e25a7d0b2daefe859c45e262c8f56f794114f9a29a0"
   head "https://github.com/aoh/owl-lisp.git"
 
   def install
