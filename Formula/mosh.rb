e5ef3e59773cb8a21f65d753afe57f100f00dfcf
Lassi Kortela <lassi@lassi.io>
2021-02-09 21:23:00 +0200

    Reformat using rufo

--- Formula/mosh.rb
+++ Formula/mosh.rb
@@ -1,13 +1,13 @@
-require 'formula'
+require "formula"
 
 class Mosh < Formula
-  homepage 'http://mosh.monaos.org'
-  url 'http://mosh-scheme.googlecode.com/files/mosh-0.2.7.tar.gz'
-  sha1 '866c08ac12e14733ce27756001a27257624d01ad'
-  head 'https://github.com/okuoku/mosh.git'
+  homepage "http://mosh.monaos.org"
+  url "http://mosh-scheme.googlecode.com/files/mosh-0.2.7.tar.gz"
+  sha1 "866c08ac12e14733ce27756001a27257624d01ad"
+  head "https://github.com/okuoku/mosh.git"
 
-  depends_on 'gmp'
-  depends_on 'oniguruma'
+  depends_on "gmp"
+  depends_on "oniguruma"
 
   fails_with :llvm do
     cause "Inline asm errors"
@@ -16,7 +16,7 @@ class Mosh < Formula
   def install
     system "sh ./gen-git-build.sh"
     system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+           "--prefix=#{prefix}"
     system "make"
     system "make check"
     system "make install"
