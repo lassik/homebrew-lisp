380fa1c050e7cb25dc684923ddebb029c6bd282c
Lassi Kortela <lassi@lassi.io>
2021-02-09 23:42:30 +0200

    Fix lint warnings from Homebrew CI

--- Formula/mosh-scheme.rb
+++ Formula/mosh-scheme.rb
@@ -1,20 +1,15 @@
-require "formula"
-
 # homebrew/core has a mosh formula for a remote terminal application.
 # Call this mosh-scheme to avoid a name clash.
 
 class MoshScheme < Formula
   homepage "http://mosh.monaos.org"
-  head "https://github.com/okuoku/mosh.git"  # higepon/mosh
+  desc "Fast Scheme interpreter [R6RS]"
   url "https://files.scheme.org/mosh-0.2.7.tar.gz"
+  head "https://github.com/okuoku/mosh.git" # higepon/mosh
   sha256 "3bafe0e3412d78041433540b59a84523f5c36c5d6c7f94a661e43d74ae7ca74e"
 
   depends_on "gmp"
   depends_on "oniguruma"
-
-  fails_with :llvm do
-    cause "Inline asm errors"
-  end
 
   def install
     system "sh ./gen-git-build.sh"
@@ -23,7 +18,7 @@ class MoshScheme < Formula
            "--prefix=#{prefix}",
            "--program-transform-name=s@mosh@mosh-scheme@; s@_@-@"
     system "make"
-    #system "make check"
+    # system "make check"
     system "make install"
   end
 end
