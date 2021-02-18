d129436ad59b9fb42902df5551c5869ec3ee5e37
Lassi Kortela <lassi@lassi.io>
2021-02-18 11:44:52 +0200

    Upgrade owl-lisp to 0.2 and gitlab head

--- Formula/owl-lisp.rb
+++ Formula/owl-lisp.rb
@@ -1,13 +1,12 @@
 class OwlLisp < Formula
   desc "Interpreter for an immutable subset of R7RS Scheme"
   homepage "https://github.com/aoh/owl-lisp"
-  url "https://github.com/aoh/owl-lisp/archive/v0.1.14.tar.gz"
-  sha256 "4d9982da3582456d1e769e25a7d0b2daefe859c45e262c8f56f794114f9a29a0"
-  head "https://github.com/aoh/owl-lisp.git"
+  url "https://gitlab.com/owl-lisp/owl/-/archive/v0.2/owl-v0.2.tar.gz"
+  sha256 "9fed7b4907883910f5a7b8d713e1e7334b20d432c5d52611b34e23b19b7edfb1"
+  head "https://gitlab.com/owl-lisp/owl.git"
 
   def install
-    bin.mkpath
-    system "make", "install", "DESTDIR=."
-    bin.install Dir["./usr/bin/*"]
+    system "make", "PREFIX=#{prefix}"
+    system "make", "PREFIX=#{prefix}", "install"
   end
 end
