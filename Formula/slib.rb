2382ee35287e160f68bc8c9be8bd6eaaa58f1c7b
Lassi Kortela <lassi@lassi.io>
2021-08-01 01:11:53 +0300

    Add slib formula

---
+++ Formula/slib.rb
@@ -0,0 +1,14 @@
+class Slib < Formula
+  desc "The SLIB Portable Scheme Library"
+  homepage "https://people.csail.mit.edu/jaffer/SLIB"
+  version "3b6"
+  url "https://files.scheme.org/slib-#{version}.zip"
+  sha256 "2e8cb4843b5400b2e39fe9ddcc8040a412f9a19cccf5e7e2c2e071ceb9b0ed8c"
+  license "GPL-3.0-or-later"  # TODO: Verify this.
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+end
