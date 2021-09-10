022858484bfe872ee0e5e03f03a32b926d25131c
Lassi Kortela <lassi@lassi.io>
2021-09-10 13:19:41 +0300

    Use s9 patch

--- Formula/scheme9.rb
+++ Formula/scheme9.rb
@@ -5,9 +5,13 @@ class Scheme9 < Formula
   sha256 "e92d6e2cd172d8f92631c0672d99e0049f52d8be1020aeda1cbecb665e5ea9c9"
   #license ""  # Public domain
 
+  patch do
+    url "https://raw.githubusercontent.com/lassik/homebrew-lisp/lassik-scheme9/patches/s9fes-20181205-make-installdirs.patch"
+    sha256 "c4c8d04804764ff9c454282e785d6025ca980aeb83527da11afd996a98e9d723"
+  end
+
   def install
     system "make", "PREFIX=#{prefix}"
-    bin.mkdir
     system "make", "PREFIX=#{prefix}", "install"
   end
 end
