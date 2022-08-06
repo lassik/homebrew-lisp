a10ef7c611a04c377508e2fd4f22afaa4509b6f3
Lassi Kortela <lassi@lassi.io>
2022-08-06 11:58:47 +0300

    Upgrade to eisl 2.60

--- Formula/eisl.rb
+++ Formula/eisl.rb
@@ -1,10 +1,10 @@
 class Eisl < Formula
   desc "ISLisp interpreter/compiler"
   homepage "https://github.com/sasagawa888/eisl"
+  version "2.60"
   url "https://github.com/sasagawa888/eisl.git",
-      tag: "v2.38",
-      revision: "16e4aa37f01fe2b914e644d87a9ce60f50015738"
-  version "2.38"
+      tag: "v#{version}",
+      revision: "88e7dfa07edc86dbe87460a613b6d8d69f1c722a"
   head "https://github.com/sasagawa888/eisl.git"
 
   def install
