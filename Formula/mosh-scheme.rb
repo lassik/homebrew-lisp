6dafa8b97b0992a84ff137b925a0aa6f1383602f
Lassi Kortela <lassi@lassi.io>
2022-09-04 20:15:21 +0300

    Tweak mosh-scheme recipe

--- Formula/mosh-scheme.rb
+++ Formula/mosh-scheme.rb
@@ -7,8 +7,12 @@ class MoshScheme < Formula
   url "https://files.scheme.org/mosh-0.2.7.tar.gz"
   sha256 "3bafe0e3412d78041433540b59a84523f5c36c5d6c7f94a661e43d74ae7ca74e"
   license "MIT"
-  head "https://github.com/okuoku/mosh.git" # higepon/mosh
+  head "https://github.com/higepon/mosh.git"
 
+  if build.head?
+    depends_on "autoconf" => :build # Only head needs this.
+    depends_on "automake" => :build # Only head needs this.
+  end
   depends_on "gmp"
   depends_on "oniguruma"
 
