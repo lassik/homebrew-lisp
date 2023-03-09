2fe1a958963e85b4e5c404afc30216962a4aebc4
Lassi Kortela <lassi@lassi.io>
2023-03-09 23:16:58 +0200

    Fix eisl `brew audit`

--- Formula/eisl.rb
+++ Formula/eisl.rb
@@ -1,9 +1,8 @@
 class Eisl < Formula
   desc "ISLisp interpreter/compiler"
   homepage "https://github.com/sasagawa888/eisl"
-  version "2.85"
   url "https://github.com/sasagawa888/eisl.git",
-      tag: "v#{version}",
+      tag:      "v2.85",
       revision: "34509bdefa88a14af142d28b864bd2deac5ffec0"
   head "https://github.com/sasagawa888/eisl.git"
 
