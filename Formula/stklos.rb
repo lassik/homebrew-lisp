7492b316bd83b959d38c3eed2058119979e802ea
Lassi Kortela <lassi@lassi.io>
2023-09-19 18:01:58 +0300

    Upgrade to STklos 2.0

--- Formula/stklos.rb
+++ Formula/stklos.rb
@@ -1,20 +1,21 @@
 class Stklos < Formula
   desc "Scheme interpreter with object-oriented GUI [R7RS]"
   homepage "https://stklos.net/"
-  url "http://www.stklos.net/download/stklos-1.70.tar.gz"
-  sha256 "15a1251a0e72d66051011bb95ba9122b22f7acd8a5a0d0e4f8737cc9fcbb83c7"
+  url "http://www.stklos.net/download/stklos-2.00.tar.gz"
+  sha256 "eb85454d2a18cefb7820157beb01ff1c84f87a1f64634e36732c938be0eec7cb"
   license "GPL-2.0-or-later"
   head "https://github.com/egallesio/STklos.git"
 
   depends_on "gmp"
   depends_on "libffi"
   depends_on "libgc"
-  depends_on "pcre"
+  #depends_on "pcre"
 
   def install
     system "./configure",
            "--disable-dependency-tracking",
-           "--prefix=#{prefix}"
+           "--prefix=#{prefix}",
+           "--with-provided-regexp"
     system "make"
     system "make", "check"
     system "make", "install"
@@ -24,7 +25,7 @@ class Stklos < Formula
     output = shell_output("#{bin}/stklos -V")
     assert_match "(version \"#{version}\")", output
     assert_match "(stklos.threads pthread)", output
-    assert_match "(stklos.system-libs libffi libpcre libgmp libgc)", output
-    assert_match "(stklos.compiled-libs)", output
+    assert_match "(stklos.system-libs libffi libgmp libgc)", output
+    assert_match "(stklos.compiled-libs libpcre)", output
   end
 end
